c148c5c8b06ba5de5fc6f2efab7145ec83c5f7fa
Obakeng Hlahle <obakenghlahle4@gmail.com>
2025-12-01 04:54:39 +0200

    Added test routes

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TEST ROUTE
+app.get("/health", (req, res) => {
+  res.send("Server running");
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is working");
+});
+
+
 // ROUTES
 app.use("/api/auth", authRoutes);
 
@@ -24,3 +34,5 @@ mongoose
 // START SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
