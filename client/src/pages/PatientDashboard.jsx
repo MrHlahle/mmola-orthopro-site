137654b8227e41f3002307e406376e09fd2e3326
Obakeng Hlahle <obakenghlahle4@gmail.com>
2026-01-22 14:33:52 +0200

    Add PatientDashboard and AdminOrders pages

---
+++ client/src/pages/PatientDashboard.jsx
@@ -0,0 +1,10 @@
+function PatientDashboard() {
+  return (
+    <div>
+      <h1>Patient Dashboard</h1>
+      <p>Your applications and device orders will appear here.</p>
+    </div>
+  );
+}
+
+export default PatientDashboard;
